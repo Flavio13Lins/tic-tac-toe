8e9f0ad90646774df2da132f87a4d635f13962a8
Flávio Lins <flmn@cin.ufpe.br>
2023-06-11 17:22:14 -0300

    feat: add dialog when game is finished

--- tic-tac-toe/src/components/HomePageComponent.vue
+++ tic-tac-toe/src/components/HomePageComponent.vue
@@ -8,10 +8,51 @@ export default {
   computed: mapState({
     started: (state) => state.gameState.started,
     paused: (state) => state.gameState.paused,
-    loading: (state) => state.gameState.loading,
+    isLoading: (state) => state.gameState.isLoading,
     finished: (state) => state.gameState.finished,
     winner: (state) => state.gameState.winner
   }),
+  data() {
+    return {
+      gameResult: false
+    }
+  },
+  watch: {
+    finished(nv) {
+      if (nv) {
+        this.gameResult = true
+      }
+    }
+  },
+  methods: {
+    async restartGame() {
+      this.gameResult = false
+      await this.$store.dispatch(`gameState/resetGame`)
+      await this.$store.dispatch(`gameState/startNewGame`)
+      await this.scrollToGame()
+    },
+    async scrollToGame() {
+      var i = 0
+      var intervalId = await setInterval(() => {
+        var currentPositionOfPage = window.scrollY
+        if (i <= 5) {
+          window.scrollTo(0, currentPositionOfPage + 10)
+          i++
+        } else if (i <= 10) {
+          window.scrollTo(0, currentPositionOfPage + 5)
+          i++
+        } else if (i <= 15) {
+          window.scrollTo(0, currentPositionOfPage + 2)
+          i++
+        } else if (i <= 20) {
+          window.scrollTo(0, currentPositionOfPage + 1)
+          i++
+        } else {
+          clearInterval(intervalId)
+        }
+      }, 100)
+    }
+  },
   components: {
     ScopedGameStateBtn,
     TableToPlay
@@ -24,23 +65,47 @@ export default {
     <v-col align="center">
       <h1>TIC TAC TOE</h1>
       <ScopedGameStateBtn
+        key="Start"
         v-if="!started"
         action="startNewGame"
-        :loading="loading"
+        @scrollToGame="scrollToGame"
       ></ScopedGameStateBtn>
       <ScopedGameStateBtn
+        @scrollToGame="_"
+        key="Reset"
         v-if="started"
-        :loading="loading"
         action="resetGame"
         icon="mdi-restart"
         size="regular"
         text="Reset"
         variant="outlined"
       ></ScopedGameStateBtn>
-      <h3 v-if="finished && winner === -1">Draw</h3>
-      <h3 v-if="finished && winner === 1">Sorry, you lose!</h3>
-      <h3 v-if="finished && winner === 0">What? YOU WON!</h3>
+      <v-dialog v-model="gameResult" width="auto">
+        <v-card min-width="280" min-height="140" class="bg-blue-lighten-3">
+          <v-row justify="space-between" no-gutters>
+            <v-spacer></v-spacer>
+            <v-card-title class="ma-4">
+              <h3 v-if="finished && winner === -1">Draw</h3>
+              <h3 v-if="finished && winner === 1">Sorry, you lose!</h3>
+              <h3 v-if="finished && winner === 0">What? YOU WON!</h3>
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-btn
+              elevation="0"
+              size="x-small"
+              class="ma-2"
+              color="blue-lighten-3"
+              icon="mdi-close"
+              @click="gameResult = false"
+            ></v-btn>
+          </v-row>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="teal-darken-4" variant="tonal" @click="restartGame">Restart</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-col>
   </v-row>
-  <TableToPlay v-if="!loading && started"></TableToPlay>
+  <TableToPlay v-if="!isLoading && started"></TableToPlay>
 </template>
