0e41075d1b2e7807922e69cae27fcec0b7401cc0
Flávio Lins <35618051+Flavio13Lins@users.noreply.github.com>
2024-04-24 22:47:59 +0000

    feat: all needed values in theme state to controll themes

--- tic-tac-toe/src/store/modules/theme/index.js
+++ tic-tac-toe/src/store/modules/theme/index.js
@@ -1,7 +1,14 @@
 export const state = () => ({
+  // Defines the value in the switch theme btn ('auto', 'dark', 'light')
+  switchThemeValue: 'auto',
+  // Defines if user already set a theme in the game
   hasPreDef: false,
+  // Defines if user set the dark theme in the game
   isDark: true,
-  theme: ''
+  // Defines the prefers-color-scheme of the users system
+  preferredTheme: '',
+  // Defines the theme current used in the game
+  theme: '',
 })
 
 export const getters = {
@@ -11,8 +18,8 @@ export const getters = {
   isDark(state) {
     return state.isDark
   },
-  preferredColor(state) {
-    return state.preferredColor
+  preferredTheme(state) {
+    return state.preferredTheme
   },
   theme(state) {
     return state.theme
@@ -26,7 +33,7 @@ export const getters = {
 }
 
 export const actions = {
-  // Create actions to change isDark, hasPref, preferredColor, theme
+  // Create actions to change switchThemeValue, isDark, hasPref, preferredTheme, theme
   list({ state }) {
     return state.list
   },
@@ -60,8 +67,8 @@ export const mutations = {
   setIsDark(state, data) {
     state.isDark = data
   },
-  setPreferredColor(state, data) {
-    state.preferredColor = data
+  setPreferredTheme(state, data) {
+    state.preferredTheme = data
   },
   setx(state, data) {
     state.x = data
